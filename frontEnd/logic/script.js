d3a8523ef3776ec8a058e296153d2ba1b63b9757
MohamadLatifi <goofySmart8@gmail.com>
2026-09-14 19:18:39 +0430

    count down the time by timer

--- frontEnd/logic/script.js
+++ frontEnd/logic/script.js
@@ -4,39 +4,26 @@ const minute = document.getElementById('minute');
 const second = document.getElementById('second');
 
 hour.innerText = new Date().getHours()
+// hour.innerText =1
 minute.innerText = new Date().getMinutes()
+// minute.innerText = 1
 second.innerText = new Date().getSeconds()
+// second.innerText = 3
 
-console.log(second.innerText)
-setTimeout(() => {
-  for (let n = 0; n < 10; n++) {
+setInterval(() => {
+
+  for (n = 0; n < 1; n++) {
     second.innerText--;
+    if (second.innerText == 0) {
+      second.innerText = 60
+      minute.innerText -= 1
+      if (minute.innerText == 0) {
+        minute.innerText = 60
+        hour.innerText -= 1
+        if (hour.innerText == 0){
+          hour.innerText=24;
+        }
+      }
+    }
   }
 }, 1000)
-
-
-// let endDay = false
-// if (hour.innerText && minute.innerText && second.innerText == 0) {
-//   endDay = true
-// }
-
-
-// function countDown() {
-//   for (let endDay=0; endDay>10 ; second.innerText) {
-//     // hour.innerText = new Date().getHours()
-//     // minute.innerText = new Date().getMinutes()
-//     second.innerText = new Date().getSeconds()
-//     console.log(second.innerText)
-//     endDay++;
-//   }
-// }
-
-// countDown()
-
-
-// let n=5;
-
-// for(let i=0;i<10;i++){
-//   n--;
-//   console.log(n)
-// }
