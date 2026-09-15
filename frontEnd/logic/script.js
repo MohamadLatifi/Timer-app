a6881de0f175c809c8947d42513f19020d4aa0e5
MohamadLatifi <goofySmart8@gmail.com>
2026-09-15 19:40:10 +0430

    main logic + isTimeToRing function

--- frontEnd/logic/script.js
+++ frontEnd/logic/script.js
@@ -1,29 +1,49 @@
-// const day = document.getElementById('day');
+const hourInput = document.getElementById('hourInput')
+const minuteInput = document.getElementById('minuteInput')
+const secondInput = document.getElementById('secondInput')
+const submit = document.getElementById('submit')
+let isSubimted = false
+submit.addEventListener('click', () => {
+
+  second.innerText = secondInput.value || 0;
+  minute.innerText = minuteInput.value || 0;
+  hour.innerText = hourInput.value || 0;
+  isSubimted = true;
+})
+
+
+
 const hour = document.getElementById('hour');
 const minute = document.getElementById('minute');
 const second = document.getElementById('second');
 
-hour.innerText = new Date().getHours()
-// hour.innerText =1
-minute.innerText = new Date().getMinutes()
-// minute.innerText = 1
-second.innerText = new Date().getSeconds()
-// second.innerText = 3
 
 setInterval(() => {
-
-  for (n = 0; n < 1; n++) {
+  if (isSubimted === true) {
     second.innerText--;
-    if (second.innerText == 0) {
+    isTimeToRing()
+    if (second.innerText < 0 && minute.innerText !=-1 ) {
       second.innerText = 60
       minute.innerText -= 1
-      if (minute.innerText == 0) {
+
+      isTimeToRing()
+      if (minute.innerText < 0 && hour.innerText!=0  ) {
         minute.innerText = 60
         hour.innerText -= 1
-        if (hour.innerText == 0){
-          hour.innerText=24;
+        isTimeToRing()
+        if (hour.innerText < 0) {
+          isTimeToRing()
         }
       }
     }
   }
 }, 1000)
+
+function isTimeToRing() {
+  if (second.innerText == 0 && minute.innerText == 0 && hour.innerText == 0) {
+    console.log('ring');
+    second.innerText = 'finished';
+    minute.innerText = 'finished ';
+    hour.innerText = 'finished';
+  }
+}
