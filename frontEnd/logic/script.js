86f214504618db674dd64d0518305572566cec18
MohamadLatifi <goofySmart8@gmail.com>
2026-09-17 11:21:10 +0430

    fixing the count down

--- frontEnd/logic/script.js
+++ frontEnd/logic/script.js
@@ -3,12 +3,16 @@ const minuteInput = document.getElementById('minuteInput')
 const secondInput = document.getElementById('secondInput')
 const submit = document.getElementById('submit')
 let isSubimted = false
+
+const ring = new Audio('/music file/dom demo.m4a'); // ساخت آبجکت صدا
+
 submit.addEventListener('click', () => {
 
   second.innerText = secondInput.value || 0;
   minute.innerText = minuteInput.value || 0;
   hour.innerText = hourInput.value || 0;
   isSubimted = true;
+  ring.play().then(() => { ring.pause(); ring.currentTime = 0; }).catch(() => { }); // پرامیس
 })
 
 
@@ -22,13 +26,13 @@ setInterval(() => {
   if (isSubimted === true) {
     second.innerText--;
     isTimeToRing()
-    if (second.innerText < 0 && minute.innerText !=-1 ) {
-      second.innerText = 60
+    if (second.innerText < 0 && minute.innerText != -1) {
+      second.innerText = 59
       minute.innerText -= 1
 
       isTimeToRing()
-      if (minute.innerText < 0 && hour.innerText!=0  ) {
-        minute.innerText = 60
+      if (minute.innerText < 0 && hour.innerText != 0) {
+        minute.innerText = 59
         hour.innerText -= 1
         isTimeToRing()
         if (hour.innerText < 0) {
@@ -41,7 +45,8 @@ setInterval(() => {
 
 function isTimeToRing() {
   if (second.innerText == 0 && minute.innerText == 0 && hour.innerText == 0) {
-    console.log('ring');
+   
+    ring.play(); // بجای لاگ
     second.innerText = 'finished';
     minute.innerText = 'finished ';
     hour.innerText = 'finished';
