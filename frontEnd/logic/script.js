e6a4c8ead551907f6a6f21021152d4feb6a8dc3b
MohamadLatifi <goofySmart8@gmail.com>
2026-09-19 21:56:22 +0430

    title name logic

--- frontEnd/logic/script.js
+++ frontEnd/logic/script.js
@@ -1,53 +1,79 @@
-const hourInput = document.getElementById('hourInput')
-const minuteInput = document.getElementById('minuteInput')
-const secondInput = document.getElementById('secondInput')
-const submit = document.getElementById('submit')
-let isSubimted = false
+// const hourInput = document.getElementById('hourInput')
+// const minuteInput = document.getElementById('minuteInput')
+// const secondInput = document.getElementById('secondInput')
+// const submit = document.getElementById('submit')
+// let isSubimted = false
 
-const ring = new Audio('/music file/dom demo.m4a'); 
+// const ring = new Audio('/music file/dom demo.m4a'); 
 
-submit.addEventListener('click', () => {
+// submit.addEventListener('click', () => {
 
-  second.innerText = secondInput.value || 0;
-  minute.innerText = minuteInput.value || 0;
-  hour.innerText = hourInput.value || 0;
-  isSubimted = true;
-  ring.play().then(() => { ring.pause(); ring.currentTime = 0; }).catch(() => {}); //ring.currentTime = 0 is for when user click submit btn sevral time 
+//   second.innerText = secondInput.value || 0;
+//   minute.innerText = minuteInput.value || 0;
+//   hour.innerText = hourInput.value || 0;
+//   isSubimted = true;
+//   ring.play().then(() => { ring.pause(); ring.currentTime = 0; }).catch(() => {}); //ring.currentTime = 0 is for when user click submit btn sevral time 
+// })
+
+
+
+// const hour = document.getElementById('hour');
+// const minute = document.getElementById('minute');
+// const second = document.getElementById('second');
+
+
+// setInterval(() => {
+//   if (isSubimted === true) {
+//     second.innerText--;
+//     isTimeToRing()
+//     if (second.innerText < 0 && minute.innerText != -1) {
+//       second.innerText = 59
+//       minute.innerText -= 1
+
+//       isTimeToRing()
+//       if (minute.innerText < 0 && hour.innerText != 0) {
+//         minute.innerText = 59
+//         hour.innerText -= 1
+//         isTimeToRing()
+//         if (hour.innerText < 0) {
+//           isTimeToRing()
+//         }
+//       }
+//     }
+//   }
+// }, 1000)
+
+// function isTimeToRing() {
+//   if (second.innerText == 0 && minute.innerText == 0 && hour.innerText == 0) {
+//     ring.play(); 
+//     second.innerText = 'finished';
+//     minute.innerText = 'finished ';
+//     hour.innerText = 'finished';
+//   }
+// }
+
+const titleForm = document.getElementById('titleForm');
+const inputForm = document.getElementById('inputForm');
+const interBtn = document.getElementById('interBtn');
+const formDiv = document.getElementById('formDiv');
+
+interBtn.addEventListener('click', () => {
+
+  const timerName = document.createElement('p')
+
+  setTimeout(() => {
+    timerName.classList.add('timer_name')
+    timerName.innerText = inputForm.value;
+    formDiv.appendChild(timerName);
+  }, 1200)
+
+  inputForm.classList.add("hide_input_form");
+  titleForm.classList.add("hide_title_form");
+  interBtn.classList.add('hide_inter_btn');
+  setTimeout(() => {
+    inputForm.remove()
+    titleForm.remove()
+    interBtn.remove()
+  }, 1000)
 })
 
-
-
-const hour = document.getElementById('hour');
-const minute = document.getElementById('minute');
-const second = document.getElementById('second');
-
-
-setInterval(() => {
-  if (isSubimted === true) {
-    second.innerText--;
-    isTimeToRing()
-    if (second.innerText < 0 && minute.innerText != -1) {
-      second.innerText = 59
-      minute.innerText -= 1
-
-      isTimeToRing()
-      if (minute.innerText < 0 && hour.innerText != 0) {
-        minute.innerText = 59
-        hour.innerText -= 1
-        isTimeToRing()
-        if (hour.innerText < 0) {
-          isTimeToRing()
-        }
-      }
-    }
-  }
-}, 1000)
-
-function isTimeToRing() {
-  if (second.innerText == 0 && minute.innerText == 0 && hour.innerText == 0) {
-    ring.play(); 
-    second.innerText = 'finished';
-    minute.innerText = 'finished ';
-    hour.innerText = 'finished';
-  }
-}
